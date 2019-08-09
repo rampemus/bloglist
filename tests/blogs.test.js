235e57b66a72f15da99030dac002070065eb0deb
rampemus <pasi.toivanen@live.com>
2019-08-09 15:40:46 +0300

    blog data added correctly -test works for hard coded values

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -4,17 +4,53 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('two existing blogs are returned as json', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body.length).toBe(2)
+const realAmountOfBlogs = 2
+
+describe('blog data retrieved correctly', () => {
+
+    test(realAmountOfBlogs + ' existing blogs are returned as json', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body.length).toBe(realAmountOfBlogs)
+    })
+
+    test('blogs have correctly named id-field', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
+
 })
 
-test('blogs have correctly named id-field', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
+describe('blog data added correctly', () => {
+    let testBlog = {
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+        likes: 10
+    }
+
+    test('Blog can be added', async() => {
+        // const response =
+        const response = await api
+            .post('/api/blogs')
+            .send(testBlog)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+        const bodyLengthResponse = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(bodyLengthResponse.body.length).toBe(realAmountOfBlogs+1)
+
+        await api
+            .delete('/api/blogs/'+response.body.id)
+            .expect(204)
+    })
 })
 
 afterAll(() => {

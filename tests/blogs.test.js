e4cc023efd4858712101d6152f7948e7a16a0c85
rampemus <pasi.toivanen@live.com>
2019-08-11 10:55:55 +0300

    Added tests for validating data and remove excess blogs after tests

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -6,6 +6,24 @@ const api = supertest(app)
 
 const realAmountOfBlogs = 2
 
+//before running the tests make sure that data in the test db is
+// {
+//     _id:5d4c156bbd1fa4efeb6594b0
+//     title:"React patterns"
+//     author:"Michael Chan"
+//     url:"https://reactpatterns.com/"
+//     likes:7
+//     __v:0
+// }
+// {
+//     _id:5d4d76eb0369c62ce3b2e4b9
+//     title:"Go To Statement Considered Harmful"
+//     author:"Edsger W. Dijkstra"
+//     url:"http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html"
+//     likes:5
+//     __v:0
+// }
+
 describe('blog data retrieved correctly', () => {
 
     test(realAmountOfBlogs + ' existing blogs are returned as json', async () => {
@@ -68,6 +86,67 @@ describe('data of blog can be added and removed', () => {
     })
 })
 
-afterAll(() => {
+describe('blogs are validated', async () => {
+
+    let responseToPost = null
+
+    test('likes will be 0 when sent as empty', async () => {
+        const blogWithoutLikes = {
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html'
+        }
+
+        responseToPost = await api
+            .post('/api/blogs')
+            .send(blogWithoutLikes)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+        await expect(responseToPost.body.likes).toBeDefined()
+    })
+
+    test('missing title returns 400', async () => {
+        const blogWithoutTitle = {
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+            likes: 10
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(blogWithoutTitle)
+            .expect(400)
+    })
+
+    test('missing url returns 400', async () => {
+        const blogWithoutUrl = {
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            likes: 10
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(blogWithoutUrl)
+            .expect(400)
+    })
+
+})
+
+afterAll( async () => {
+
+    const response = await api
+        .get('/api/blogs')
+
+    for ( let i = 0; i < response.body.length; i++ ) {
+        if ( response.body[i].id.toString() !== '5d4c156bbd1fa4efeb6594b0'
+            && response.body[i].id.toString() !== '5d4d76eb0369c62ce3b2e4b9' ) {
+            await api
+                .delete('/api/blogs/'+response.body[i].id)
+        }
+    }
+
     mongoose.connection.close()
 })
